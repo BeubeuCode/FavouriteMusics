e1499dd348be714e06715cc6d5d0a5ea0b9abc81
Benoît Arn <benoit.arnlt@gmail.com>
2020-09-30 14:07:19 +0200

    margin bottom on button

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,6 +7,7 @@
                 Form follows fucking function. Nothing of value comes to you without fucking working at it. Never let your guard down by thinking you’re fucking good enough. This design is fucking brilliant. Learn from fucking criticism. Design as if your fucking life depended on it. The details are not the details. They make the fucking design. Creativity is a fucking work-ethic. If you fucking give up, you will achieve nothing. To surpass others is fucking tough, if you only do as you are told you don’t have it in you to succeed. Dedicate yourself to lifelong fucking learning. While having drinks with Tibor Kalman one night, he told me, “When you make something no one hates, no one fucking loves it.” Remember it’s called the creative process, it’s not the creative fucking moment. Don’t worry about what other people fucking think. Never, never assume that what you have achieved is fucking good enough. Someday is not a fucking day of the week. Don’t fucking lie to yourself. Practice won’t get you anywhere if you mindlessly fucking practice the same thing. Change only occurs when you work deliberately with purpose toward a goal. You need to sit down and sketch more fucking
             </p>
             <a href="#" class="btn btn-colored">Créer mon compte</a> <a href="#" class="btn btn-colored">Me connecter</a>
+            <br/>
         </div>
         <div class="col-md-6 centered">
             <img src="/svg/listening.svg" class="svgLanding">
