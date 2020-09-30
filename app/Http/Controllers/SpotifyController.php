7c9343c5316049690a024f0000a255295e12a8d2
Benoît Arn <benoit.arnlt@gmail.com>
2020-09-30 15:48:27 +0200

    added Spotify API and searched for sabaton

---
+++ app/Http/Controllers/SpotifyController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Support\Facades\Response;
+use SpotifyWebAPI;
+
+class SpotifyController extends Controller
+{
+    private $CLIENT_ID = '7d857c333a7644b1871988b1a6d75a81';
+    private $CLIENT_SECRET = 'f347df3dd9084b158c5a0b8f0c0021ea';
+
+    public function connect() {
+        $session = new SpotifyWebAPI\Session(
+            $this->CLIENT_ID, //id
+            $this->CLIENT_SECRET //secret
+        );
+        $session->requestCredentialsToken();
+        return $session->getAccessToken();
+    }
+
+    public function searchResults($query, $type, $token) {
+        $api = new SpotifyWebAPI\SpotifyWebAPI();
+        $api->setAccessToken($token);
+        return $api->search(
+            $query,
+            $type
+        );
+    }
+
+    public function connectAndSearch() {
+        //dummy
+        $token = $this->connect();
+        $results = $this->searchResults('Sabaton', 'artist', $token);
+        return Response::json($results);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SpotifyController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 });
+
+Route::get('/callback', [SpotifyController::class, 'connectAndSearch']);
