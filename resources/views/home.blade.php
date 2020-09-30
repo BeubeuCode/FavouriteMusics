db9bebd7289b84f3ec3e16a8b6c2491bbcb8f3a5
Benoît Arn <benoit.arnlt@gmail.com>
2020-09-30 14:16:20 +0200

    changed title tag and styling

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,7 +2,7 @@
 @section('content')
     <div class="row">
         <div class="col-md-6">
-            <h1>Partagez les musiques que vous aimez.</h1>
+            <h2 class="title">Partagez les musiques que vous aimez.</h2>
             <p>
                 Form follows fucking function. Nothing of value comes to you without fucking working at it. Never let your guard down by thinking you’re fucking good enough. This design is fucking brilliant. Learn from fucking criticism. Design as if your fucking life depended on it. The details are not the details. They make the fucking design. Creativity is a fucking work-ethic. If you fucking give up, you will achieve nothing. To surpass others is fucking tough, if you only do as you are told you don’t have it in you to succeed. Dedicate yourself to lifelong fucking learning. While having drinks with Tibor Kalman one night, he told me, “When you make something no one hates, no one fucking loves it.” Remember it’s called the creative process, it’s not the creative fucking moment. Don’t worry about what other people fucking think. Never, never assume that what you have achieved is fucking good enough. Someday is not a fucking day of the week. Don’t fucking lie to yourself. Practice won’t get you anywhere if you mindlessly fucking practice the same thing. Change only occurs when you work deliberately with purpose toward a goal. You need to sit down and sketch more fucking
             </p>
