18baedd27f0663073b5e35ced629303eec825830
Benoît Arn <benoit.arnlt@gmail.com>
2020-10-02 11:36:07 +0200

    checking password equality, need actual code

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,13 @@
 require('./bootstrap');
+
+firstField = document.getElementById("password1");
+secondField = document.getElementById('password2')
+
+function checkPasswordEquality() {
+    if(firstField.value() === secondField.value()) {
+        //c'est ok
+    } else {
+        //pas bon
+    }
+}
+
