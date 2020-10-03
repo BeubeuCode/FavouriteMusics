232be7f192cb69b597be1a5ced2c4c688ffc9535
Benoît Arn <benoit.arnlt@gmail.com>
2020-10-03 15:00:20 +0200

    profile modifications (image, layout on PC...)

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -3,16 +3,39 @@
 namespace App\Http\Controllers;
 
 use App\Models\LikedGenres;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Response;
 
 class ProfileController extends Controller
 {
+    private function getGravatarProfilePicture($email, $size) {
+        $emailHash = md5(strtolower(trim($email)));
+        return 'https://www.gravatar.com/avatar/'. $emailHash . '?s=' . $size ;
+    }
+
     public function showProfile() {
         $user = Auth::user();
         if($user === null) { return Response::redirectTo('/login'); }
         $genres = LikedGenres::where('user_id', $user->id)->first();
-        return Response::view('profile.profile', compact(['user', 'genres']));
+        $profilePicture = $this->getGravatarProfilePicture($user->email, 150);
+        if(!$genres) {
+            return Response::view('profile.profile', compact(['user', 'profilePicture']));
+        } else {
+            return Response::view('profile.profile', compact(['user', 'genres', 'profilePicture']));
+        }
+    }
+
+    public function showAnotherUsersProfile($username) {
+        $user = User::where('name', $username)->first();
+        if($user === null) { return abort(404); }
+        $genres = LikedGenres::where('user_id', $user->id)->first();
+        $profilePicture = $this->getGravatarProfilePicture($user->email, 150);
+        if(!$genres) {
+            return Response::view('profile.profile', compact(['user', 'profilePicture']));
+        } else {
+            return Response::view('profile.profile', compact(['user', 'genres', 'profilePicture']));
+        }
     }
 }
--- resources/views/profile/profile.blade.php
+++ resources/views/profile/profile.blade.php
@@ -1,19 +1,42 @@
 @extends('template')
 @section('title') Mon profil @endsection
 @section('content')
-    <h1 class="title">Profil</h1>
-    <p class="paragraph">Bonjour, {{$user->name}} !</p>
     <hr>
-    <h3 class="title">Genres Favoris</h3>
-    <p>
-        @php $genres_arr = explode(',', $genres->favgenres) @endphp
-        @foreach($genres_arr as $el)
-            @if(next($genres_arr) !== false)
-                {{ $el  }},
+    <div class="row">
+        <div class="col-md-4">
+            <div class="centered">
+                <img src="{{$profilePicture}}" alt="profile_picture" class="circleImage">
+                <h1 class="title">Profil</h1>
+                <p class="paragraph">Bonjour, {{$user->name}} !</p>
+            </div>
+
+        </div>
+        <div class="col-md-4">
+            <h3 class="title">Genres Favoris</h3>
+            @if(isset($genres))
+            <p class="paragraph">
+                @php $genres_arr = explode(',', $genres->favgenres) @endphp
+                @foreach($genres_arr as $el)
+                    @if(next($genres_arr) !== false)
+                        {{ $el }},
+                    @else
+                        {{$el}}
+                    @endif
+                @endforeach
+            </p>
             @else
-                {{$el}}
+                <p class="paragraph">Pas de genres favoris !</p>
             @endif
-        @endforeach
-    </p>
+        </div>
+
+        <div class="col-md-4">
+            <h3 class="title">Musiques favorites</h3>
+            <p class="paragraph">Pas de musiques favorites !</p>
+        </div>
+    </div>
+
+
+    <hr>
+
 
 @endsection
