8d57ccaefc5317b23d1ca1d945fb2453662433c5
Benoît Arn <benoit.arnlt@gmail.com>
2020-10-05 10:32:38 +0200

    checking user ID in profile.blade.php for params

--- resources/views/profile/profile.blade.php
+++ resources/views/profile/profile.blade.php
@@ -46,33 +46,34 @@
     </div>
     <hr>
     @if(\Illuminate\Support\Facades\Auth::user() != null)
-        <h2 class="title">Paramètres</h2>
+        @if(\Illuminate\Support\Facades\Auth::id() == $user->id)
+            <h2 class="title">Paramètres</h2>
 
-        <div class="row" id="settings">
-            <div class="col-md-6">
-                <h3 class="title">Genres favoris</h3>
-                <div class="form-group">
-                    <label for="newGenreInput">Enelver un genre : </label>
-                    <input class="form-control" type="text" id="oldGenreInput" name="newGenreInput">
-                    <br>
-                    <button class="btn btn-colored" id="removeGenreButton">Enlever</button>
+            <div class="row" id="settings">
+                <div class="col-md-6">
+                    <h3 class="title">Genres favoris</h3>
+                    <div class="form-group">
+                        <label for="newGenreInput">Enelver un genre : </label>
+                        <input class="form-control" type="text" id="oldGenreInput" name="newGenreInput">
+                        <br>
+                        <button class="btn btn-colored" id="removeGenreButton">Enlever</button>
+                    </div>
+                    <div class="form-group">
+                        <label for="newGenreInput">Ajouter un genre : </label>
+                        <input class="form-control" type="text" id="newGenreInput" name="newGenreInput">
+                        <br>
+                        <button class="btn btn-colored" id="addGenreButton">Ajouter</button>
+                    </div>
                 </div>
-                <div class="form-group">
-                    <label for="newGenreInput">Ajouter un genre : </label>
-                    <input class="form-control" type="text" id="newGenreInput" name="newGenreInput">
-                    <br>
-                    <button class="btn btn-colored" id="addGenreButton">Ajouter</button>
+                <div class="col-md-6">
+                    <h3 class="title">Musiques favorites</h3>
+                    <div class="form-group">
+                        <label for="newMusicInput">Ajouter une musique : </label>
+                        <input type="text" class="form-control" name="newMusicInput" id="newMusicInput">
+                        <br>
+                    </div>
                 </div>
             </div>
-            <div class="col-md-6">
-                <h3 class="title">Musiques favorites</h3>
-                <div class="form-group">
-                    <label for="newMusicInput">Ajouter une musique : </label>
-                    <input type="text" class="form-control" name="newMusicInput" id="newMusicInput">
-                    <br>
-                </div>
-            </div>
-        </div>
-
+        @endif
     @endif
 @endsection
