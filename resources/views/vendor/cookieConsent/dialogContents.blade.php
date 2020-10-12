284d3844e5ddb6b69497ef2617144c059e9aca0a
Benoît Arn <benoit.arnlt@gmail.com>
2020-10-12 22:10:59 +0200

    changed colors gpdr consent

--- resources/views/vendor/cookieConsent/dialogContents.blade.php
+++ resources/views/vendor/cookieConsent/dialogContents.blade.php
@@ -4,7 +4,7 @@
         Afin de vous offrir une expérience de navigation optimale, ce site utilise des cookies.&nbsp;<a href="/legal" target="_blank">En savoir plus</a>
     </span>
 
-    <button class="js-cookie-consent-agree cookie-consent__agree">
+    <button aria-label="fermer RGPD" class="js-cookie-consent-agree cookie-consent__agree">
         {{-- Continuer --}}<i class="fas fa-times"></i>
     </button>
 
